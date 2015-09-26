caa94b921367b788fea1198d90b0f7442f38a24e
samdoiron <samdoiron@fastmail.fm>
2015-09-26 13:02:57 -0300

    Add basic css layout
    Also, switch to development React for better message

--- frontend/js/app.jsx
+++ frontend/js/app.jsx
@@ -1,9 +1,30 @@
 
 var App = React.createClass({
   render: function () {
-    return <h1>We did it Reddit!</h1>
+    return (
+      <div className="app">
+        <TitleBar />
+        <div className="dashboard">
+          <div className="box-wide foo"></div>
+          <div className="box bar"></div>
+          <div className="box foo"></div>
+          <div className="box bar"></div>
+          <div className="box foo"></div>
+          <div className="box bar"></div>
+          <div className="box foo"></div>
+          <div className="box bar"></div>
+          <div className="box foo"></div>
+          <div className="box bar"></div>
+          <div className="box foo"></div>
+          <div className="box bar"></div>
+          <div className="box foo"></div>
+          <div className="box bar"></div>
+          <div className="box foo"></div>
+        </div>
+      </div>
+    )
   }
 });
 
-React.render(<App />, document.getElementById('app'));
+React.render(<App />, document.getElementsByClassName('app')[0]);
 
