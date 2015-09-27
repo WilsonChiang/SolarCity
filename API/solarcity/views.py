cd984b61e18e28012d4d7e3756bad3efdf9c0862
tdyer <tdyer@aio-tv.com>
2015-09-26 21:20:11 -0300

    start returning detailed home data

--- API/solarcity/views.py
+++ API/solarcity/views.py
@@ -20,29 +20,79 @@ class MoneyViewSet(viewsets.ReadOnlyModelViewSet):
         qs = models.Energy.objects.filter(wel=home.wel_address)
 
         if min_time and int(min_time):
-            min_time = datetime.datetime.fromtimestamp(int(min_time))
-            qs = qs.filter(sample_time__gte=min_time)
+            min_time = int(min_time)
+            min_datetime = datetime.datetime.fromtimestamp(min_time)
+            qs = qs.filter(sample_time__gte=min_datetime)
 
         if max_time and int(max_time):
-            max_time = datetime.datetime.fromtimestamp(int(max_time))
-            qs = qs.filter(sample_time__lte=max_time)
-
-        base_energy_cost = utils.cost_of_energy(utils.energy(sum([energy.power_to_heat_total for energy in qs[:60]]), 1),
-                                                 home.fuel_cost)
-        solar_energy_cost = utils.cost_of_energy(utils.energy(sum([energy.power_to_solar_tank for energy in qs[:60]]), 1),
-                                           home.fuel_cost)
-        new_cost = base_energy_cost - solar_energy_cost
+            max_time = int(max_time)
+            max_datetime = datetime.datetime.fromtimestamp(max_time)
+            qs = qs.filter(sample_time__lte=max_datetime)
 
         energies = list(qs)
+        base_costs = []
+        solar_savings = []
+        new_costs = []
+        for i in range((max_time - min_time) / (self._get_factor_based_on_step(step))):
+            my_energies = energies[i * self._get_other_step(step):(i + 1) * self._get_other_step(step)]
+            if len(my_energies) == 0:
+                break
+            base_energy_cost = utils.cost_of_energy(sum([energy.power_to_heat_total for energy in my_energies]),
+                                                    home.fuel_cost)
+            solar_energy_cost = utils.cost_of_energy(sum([energy.power_to_solar_tank for energy in my_energies]),
+                                                     home.fuel_cost)
+            new_cost = base_energy_cost - solar_energy_cost
+            base_costs.append(base_energy_cost)
+            solar_savings.append(solar_energy_cost)
+            new_costs.append(new_cost)
 
-        for i in range((max_time-min_time)/60):
-            if i % 60 == 0:
-                # do the things
-                pass
-            energy_accumulator += energy.power_to_solar_tank
-            pass
+        average_base = sum(base_costs)/len(base_costs)*1.0
+        average_solar = sum(solar_savings)/len(solar_savings)*1.0
+        average_new = sum(new_costs)/len(new_costs)*1.0
+        foo = {
+            'home':{
+                'average_old': average_base,
+                'average_solar': average_solar,
+                'average_new': average_new,
+                'count': len(new_costs),
+                'new_costs': new_costs,
+                'solar_savings': solar_savings,
+                'base_costs': base_costs,
+            }
+        }
+        # json.loads(foo)
 
-        return Response(new_cost)
+        return Response(foo)
+
+    def _get_factor_based_on_step(self, step):
+        step_size = 60 * 60
+        if step == 'hour':
+            return step_size
+        step_size *= 24
+        if step == 'daily':
+            return step_size
+        # weekly
+        return step_size * 7
+
+    def _get_other_step(self, step):
+        step_size = 60
+        if step == 'hour':
+            return step_size
+        step_size *= 24
+        if step == 'daily':
+            return step_size
+        # weekly
+        return step_size * 7
+
+    def _get_other_other_step(self, step):
+        step_size = 1
+        if step == 'hour':
+            return step_size
+        step_size *= 24
+        if step == 'daily':
+            return step_size
+        # weekly
+        return step_size * 7
 
 
 class HomesViewSet(viewsets.ReadOnlyModelViewSet):
