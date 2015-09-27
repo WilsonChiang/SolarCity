b5e6be620d9602c67812e0ea836a20adbbb8b987
nathanmyles <nmyles@aio-tv.com>
2015-09-27 09:41:15 -0300

    Guard against null data

--- API/solarcity/utils.py
+++ API/solarcity/utils.py
@@ -7,6 +7,8 @@ def energy(power, time):
     :param time:  time in hours
     :return: kWh
     """
+    power = power or 0.0
+    time = time or 0.0
     return power * time
 
 
@@ -27,6 +29,7 @@ def heat_sun(solar_power, collector_type):
     else:
         raise Exception('Invalid collector_type provided')
 
+    solar_power = solar_power or 0.0
     return (collector_area * solar_power) / 1000
 
 
@@ -37,7 +40,9 @@ def power_to_solar_tank(flow_gly, t_hx_gly_in, t_hx_gly_out):
     :param t_hx_gly_out: Temperature of the glycol exiting the heat exchanger. (C)
     :return: Power to solar tank from heat exchanger (kW)
     """
-
+    flow_gly = flow_gly or 0.0
+    t_hx_gly_in = t_hx_gly_in or 0.0
+    t_hx_gly_out = t_hx_gly_out or 0.0
     return flow_gly * ((t_hx_gly_in - t_hx_gly_out) * (64.2/60000))
 
 
@@ -48,7 +53,9 @@ def power_to_aux_tank(flow_water, t_water_solar, t_water_cold):
     :param t_water_cold: Temperature of the water entering the solar tank. (C)
     :return: Power to auxiliary heater from solar tank (kW)
     """
-
+    flow_water = flow_water or 0.0
+    t_water_solar = t_water_solar or 0.0
+    t_water_cold = t_water_cold or 0.0
     return flow_water * ((t_water_solar - t_water_cold) * (69.8/60000))
 
 
@@ -59,7 +66,9 @@ def power_to_heat_water_total(flow_water, t_water_hot, t_water_cold):
     :param t_water_cold: Temperature of the water entering the solar tank. (C)
     :return: Total power to heat domestic hot water (kW)
     """
-
+    flow_water = flow_water or 0.0
+    t_water_hot = t_water_hot or 0.0
+    t_water_cold = t_water_cold or 0.0
     return flow_water * ((t_water_hot-t_water_cold) * (69.8/60000))
 
 
@@ -69,6 +78,8 @@ def cost_of_energy(energy, fuel_cost):
     :param fuel_cost: $/kWh
     :return: Cost of energy ($)
     """
+    energy = energy or 0.0
+    fuel_cost = fuel_cost or 0.0
     return energy * fuel_cost
 
 
@@ -78,4 +89,6 @@ def co2_emissions(energy, co2):
     :param co2: emissions in kg/kWh
     :return: kg
     """
+    energy = energy or 0.0
+    co2 = co2 or 0.0
     return energy * co2
